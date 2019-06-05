796c1fbb0248a9d9832d1b4e416ef6aad352f4d6
ericheep <ericheep@gmail.com>
2019-06-04 21:58:48 -0700

    :art: fix indentation

--- cpx_transmit/cpx_transmit.ino
+++ cpx_transmit/cpx_transmit.ino
@@ -2,7 +2,7 @@
 
 void setup() {
 	CircuitPlayground.begin();
-  Serial1.begin(115200);
+	Serial1.begin(115200);
 }
 
 bool leftButtonPressed;
@@ -12,35 +12,35 @@ byte bytes[9];
 int loopDelay = 50;
 
 byte scaleReading(float reading, int offset, int divide) {
- byte byteReading = byte((reading + offset) / divide * 0xff);
-  
-  if (byteReading <= 0) {
-    return 0;
-  }
-  if (byteReading > 254) {
-    return 254;
-  }
-  return byteReading;
+	byte byteReading = byte((reading + offset) / divide * 0xff);
+
+	if (byteReading <= 0) {
+		return 0;
+	}
+	if (byteReading > 254) {
+		return 254;
+	}
+	return byteReading;
 
 }
 
 void loop() {
-  byte lightValue = CircuitPlayground.lightSensor();
-  leftButtonPressed = CircuitPlayground.leftButton();
-  rightButtonPressed = CircuitPlayground.rightButton();
-  byte xReading = scaleReading(CircuitPlayground.motionX(), 10, 20);
-  byte yReading = scaleReading(CircuitPlayground.motionY(), 10, 20);
-  byte zReading = scaleReading(CircuitPlayground.motionZ(), 10, 20);
-  byte soundReading = scaleReading(CircuitPlayground.mic.soundPressureLevel(10), -50, 50);
-  
-  bytes[0] = 0xff;
-  bytes[1] = lightValue;
-  bytes[2] = leftButtonPressed;
-  bytes[3] = rightButtonPressed;
-  bytes[4] = xReading;
-  bytes[5] = yReading;
-  bytes[6] = zReading;
-  bytes[7] = soundReading;
+	byte lightValue = CircuitPlayground.lightSensor();
+	leftButtonPressed = CircuitPlayground.leftButton();
+	rightButtonPressed = CircuitPlayground.rightButton();
+	byte xReading = scaleReading(CircuitPlayground.motionX(), 10, 20);
+	byte yReading = scaleReading(CircuitPlayground.motionY(), 10, 20);
+	byte zReading = scaleReading(CircuitPlayground.motionZ(), 10, 20);
+	byte soundReading = scaleReading(CircuitPlayground.mic.soundPressureLevel(10), -50, 50);
+
+	bytes[0] = 0xff;
+	bytes[1] = lightValue;
+	bytes[2] = leftButtonPressed;
+	bytes[3] = rightButtonPressed;
+	bytes[4] = xReading;
+	bytes[5] = yReading;
+	bytes[6] = zReading;
+	bytes[7] = soundReading;
 
 	Serial1.write(bytes, 9);
 	delay(loopDelay);
