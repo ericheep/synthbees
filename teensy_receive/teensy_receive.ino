42395796f5ca02217729c9f19fd30edfb4f5a50b
ericheep <ericheep@gmail.com>
2019-06-04 22:04:25 -0700

    :art: change names of bytes in array

--- teensy_receive/teensy_receive.ino
+++ teensy_receive/teensy_receive.ino
@@ -3,31 +3,35 @@
 char bytes[7];
 
 void setup() {
-	Serial.begin(9600);
-	HWSERIAL.begin(115200);
-	Serial.print("hello");
+  Serial.begin(9600);
+  HWSERIAL.begin(115200);
+  Serial.print("hello");
 }
 
 void loop() {
-	if (HWSERIAL.available()) {
-		if (HWSERIAL.read() == 0xff) {
-			HWSERIAL.readBytes(bytes, 7);
+  if (HWSERIAL.available()) {
+    if (HWSERIAL.read() == 0xff) {
+      HWSERIAL.readBytes(bytes, 7);
 
-			Serial.print("UART received: ");
-			Serial.print(bytes[0], DEC);
-			Serial.print(", ");
-			Serial.print(bytes[1], DEC);
-			Serial.print(", ");
-			Serial.print(bytes[2], DEC);
-			Serial.print(", ");
-			Serial.print(bytes[3], DEC);
-			Serial.print(", ");
-			Serial.print(bytes[4], DEC);
-			Serial.print(", ");
-			Serial.print(bytes[5], DEC);
-			Serial.print(", ");
-			Serial.println(bytes[6], DEC);
+      byte lightReading = byte(bytes[0]);
+      byte leftButtonPressed = byte(bytes[1]);
+      byte rightButtonPressed = byte(bytes[2]);
 
-		}
-	}
+      Serial.print("UART received: ");
+      Serial.print(lightReading);
+      Serial.print(", ");
+      Serial.print(leftButtonPressed);
+      Serial.print(", ");
+      Serial.print(rightButtonPressed);
+      Serial.print(", ");
+      Serial.print(bytes[3], DEC);
+      Serial.print(", ");
+      Serial.print(bytes[4], DEC);
+      Serial.print(", ");
+      Serial.print(bytes[5], DEC);
+      Serial.print(", ");
+      Serial.println(bytes[6], DEC);
+
+    }
+  }
 }
